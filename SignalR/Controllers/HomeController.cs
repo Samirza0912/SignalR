8941514df4916d86702d0c48e57ae4fb529c6a9f
Samirza0912 <samirza@code.edu.az>
2022-08-03 15:55:47 +0400

    feat:format

---
+++ SignalR/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SignalR.Models;
+using SignalR.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public HomeController( UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ILogger<HomeController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult Chat()
+        {
+            return View();
+        }
+        public IActionResult CreateUser()
+        {
+            var result = _userManager.CreateAsync(new AppUser { UserName = "Samir", Fullname = "Samir" }, "12345@Sa").Result;
+            var result2 = _userManager.CreateAsync(new AppUser { UserName = "Orxan", Fullname = "Orxan" }, "12345@Sa").Result;
+            var result3 = _userManager.CreateAsync(new AppUser { UserName = "Reshid", Fullname = "Reshid" }, "12345@Sa").Result;
+            var result4 = _userManager.CreateAsync(new AppUser { UserName = "Ferid", Fullname = "Ferid" }, "12345@Sa").Result;
+            return Ok("Users created");
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM login)
+        {
+            var user = _userManager.FindByNameAsync(login.Username).Result;
+            var result = await _signInManager.PasswordSignInAsync(user, login.Password, true, true);
+            return RedirectToAction("chat");
+        }
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.DAL;
+using SignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly AppDbContext _context;
+        public ChatHub(UserManager<AppUser> userManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", user, message, DateTime.Now.ToString("dd.MM.yyyy"));
+        }
+        public override Task OnConnectedAsync()
+        {
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                AppUser user = _userManager.FindByNameAsync(Context.User.Identity.Name).Result;
+                user.ConnectionId = Context.ConnectionId;
+                _context.SaveChanges();
+            }
+
+            return base.OnConnectedAsync();
+        }
+    }
+}
